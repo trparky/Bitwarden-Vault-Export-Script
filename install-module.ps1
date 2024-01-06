91f350071e7bebee6c65cc835e52d4d53484dd74
Thomas Parkison <trparky@toms-world.org>
2024-01-06 18:56:53 -0500

    Renamed some variables in the module installer script.

--- install-module.ps1
+++ install-module.ps1
@@ -1,5 +1,5 @@
-$path = Join-Path (Get-Location) "Export-Bitwarden.psd1"
-$contentToAppend = "`n`$ExportBitwardenScriptPath = ""$path""
+$pathToPSD1File = Join-Path (Get-Location) "Export-Bitwarden.psd1"
+$contentToAppend = "`n`$ExportBitwardenScriptPath = ""$pathToPSD1File""
 if (Test-Path(`$ExportBitwardenScriptPath)) { Import-Module `$ExportBitwardenScriptPath }"
 
 if (!(Test-Path -Path $PROFILE)) {
@@ -15,12 +15,12 @@ else {
 		if ($file_contents -cmatch '\$ExportBitwardenScriptPath = "([]\t !"#$%&''()*+,./0-9:;<=>?@A-Z[\\_`a-z{|}~^-]{10,}\.psd1)"') {
 			$ExportBitwardenScriptPath = $matches[1]
 
-			if ($ExportBitwardenScriptPath.ToLower().Trim() -eq $path.ToLower()) {
+			if ($ExportBitwardenScriptPath.ToLower().Trim() -eq $pathToPSD1File.ToLower()) {
 				Write-Host "Module already installed."
 			}
 			else {
 				Write-Host "Module path found, but not correct. Correcting path..." -NoNewline
-				$file_contents = $file_contents -creplace '\$ExportBitwardenScriptPath = "([]\t !"#$%&''()*+,./0-9:;<=>?@A-Z[\\_`a-z{|}~^-]{10,}\.psd1)"', "`$ExportBitwardenScriptPath = ""$path"""
+				$file_contents = $file_contents -creplace '\$ExportBitwardenScriptPath = "([]\t !"#$%&''()*+,./0-9:;<=>?@A-Z[\\_`a-z{|}~^-]{10,}\.psd1)"', "`$ExportBitwardenScriptPath = ""$pathToPSD1File"""
 				$file_contents | Out-File -FilePath $PROFILE -NoNewline
 				Write-Host " Done. You will now have to restart the Powershell shell for the changes to take effect."
 			}
