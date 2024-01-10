0982d218abc82d2d15b7a871108517b563f7b0d4
Thomas Parkison <trparky@toms-world.org>
2024-01-10 18:44:07 -0500

    Updated the version.

--- Export-Bitwarden.psm1
+++ Export-Bitwarden.psm1
@@ -26,7 +26,7 @@ function Export-Bitwarden { # Don't touch this line!
 		# ====================================================
 
 		Write-Host -ForegroundColor Green "========================================================================================"
-		Write-Host -ForegroundColor Green "==                        Bitwarden Vault Export Script v1.25                         =="
+		Write-Host -ForegroundColor Green "==                        Bitwarden Vault Export Script v1.26                         =="
 		Write-Host -ForegroundColor Green "== Originally created by David H, converted to a Powershell Script by Thomas Parkison =="
 		Write-Host -ForegroundColor Green "==              https://github.com/trparky/Bitwarden-Vault-Export-Script              =="
 		Write-Host -ForegroundColor Green "========================================================================================"
