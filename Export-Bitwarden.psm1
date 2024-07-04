2e84ed5780a20b7a10f4721393e31241d69b8a71
Thomas Parkison <trparky@toms-world.org>
2024-07-04 10:09:23 -0400

    Updated the Regex pattern to validate email addresses.

--- Export-Bitwarden.psm1
+++ Export-Bitwarden.psm1
@@ -94,7 +94,8 @@ function Export-Bitwarden { # Don't touch this line!
 				[string]$email
 			)
 
-			$emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+			# Pattern comes from https://emailregex.com/index.html
+			$emailPattern = '\A(?:(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))\Z'
 			$regex = New-Object System.Text.RegularExpressions.Regex $emailPattern
 
 			return $regex.IsMatch($email)
